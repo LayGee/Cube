4deebb782330192ae23fd7941c1cb4dd6a30d4c3
Lay Gee Cunanan <111631973+LayGee@users.noreply.github.com>
2022-12-19 21:51:45 +0800

    Add files via upload

---
+++ Assets/Rotator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(5f, 10f * Time.deltaTime, 0f, Space.Self);
+    }
+}
